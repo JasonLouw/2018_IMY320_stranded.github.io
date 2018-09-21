112bea7e597ffe8ef31ff823277403aa427db763
HappyTime314 <kyleoosthuizen1321@gmail.com>
2018-09-21 13:47:15 +0200

    Add files via upload

--- Website/Scripts/scroll.js
+++ Website/Scripts/scroll.js
@@ -2,7 +2,7 @@ $(function()
 {
 	$("#devLink").on('click', function() 
 	{
-		console.log("Hello"); 
+		//console.log("Hello"); 
 		var position = $("#dev").offset().top; 
 		$("HTML, BODY").animate({ scrollTop: position }, 1000); 
 	});
@@ -12,7 +12,7 @@ $(function()
 {
 	$("#headingLink").on('click', function() 
 	{
-		console.log("Hello"); 
+		//console.log("Hello"); 
 		var position = $("#heading").offset().top; 
 		$("HTML, BODY").animate({ scrollTop: position }, 1000); 
 	});
@@ -22,8 +22,32 @@ $(function()
 {
 	$("#orderLink").on('click', function() 
 	{
-		console.log("Hello"); 
+		//console.log("Hello"); 
 		var position = $("#order").offset().top; 
 		$("HTML, BODY").animate({ scrollTop: position }, 1000); 
+	});
+});
+
+$(document).on("scroll", function()
+{
+	//console.log("Hello");
+	var y = $(this).scrollTop();
+	if(y >= $("#navigation").offset().top)
+	{
+		$("#up").fadeIn();
+	}
+	else
+	{
+		$("#up").fadeOut();
+	}
+});
+
+$(function() 
+{
+	$("#up").on('click', function() 
+	{
+		//console.log("Hello"); 
+		var position = $("#headingLink").offset().top; 
+		$("HTML, BODY").animate({ scrollTop: 0 }, 1000); 
 	});
 });
